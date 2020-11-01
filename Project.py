7aeb4c43612686fc5d4b21cc472a7da267f71daa
DESKTOP-2N9E9U9\Олександр <7kozakv@gmail.com>
2020-11-01 13:27:44 +0200

    Pads and ball

--- Project.py
+++ Project.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+import random
+
 WIDTH = 900
 HEIGHT = 300
 
@@ -11,16 +13,14 @@ BALL_RADIUS = 30
 BALL_X_CHANGE = 20
 BALL_Y_CHANGE = 0
 
-root = Tk()
-root.title("Ping Pong")
+window = Tk()
+window.title("Ping Pong")
 
-c = Canvas(root, width=WIDTH, height=HEIGHT, background="#008B8B")
+c = Canvas(window, width=WIDTH, height=HEIGHT, background="#008B8B")
 c.pack()
 
 BALL = c.create_oval(WIDTH/2-BALL_RADIUS/2, HEIGHT/2-BALL_RADIUS/2, WIDTH/2+BALL_RADIUS/2, HEIGHT/2+BALL_RADIUS/2, fill="white")
-
 LEFT_PAD = c.create_line(PAD_W/2, 0, PAD_W/2, PAD_H, width=PAD_W, fill="#Da70D6")
-
 RIGHT_PAD = c.create_line(WIDTH-PAD_W/2, 0, WIDTH-PAD_W/2, PAD_H, width=PAD_W, fill="#Da70D6")
 
 PAD_SPEED = 20
@@ -31,18 +31,51 @@ BALL_SPEED_UP = 1.00
 BALL_MAX_SPEED = 30
 BALL_X_SPEED = 20
 BALL_Y_SPEED = 20
-right_line_distance = WIDTH - PAD_W
+right_line_distance = WIDTH - PAD_W   # відстань до правого краю
+
+
+def bounce(action):
+    global BALL_X_SPEED, BALL_Y_SPEED
+    if action == "strike":
+        BALL_Y_SPEED = random.randrange(-10, 10)
+        if abs(BALL_X_SPEED) < BALL_MAX_SPEED:
+            BALL_X_SPEED *= -BALL_SPEED_UP
+        else:
+            BALL_X_SPEED = -BALL_X_SPEED
+    else:
+        BALL_Y_SPEED = -BALL_Y_SPEED
+
 
 def move_ball():
     ball_left, ball_top, ball_right, ball_bot = c.coords(BALL)
     ball_center = (ball_top + ball_bot) / 2
+    if ball_right + BALL_X_SPEED < right_line_distance and ball_left + BALL_X_SPEED > PAD_W:
+        c.move(BALL, BALL_X_SPEED, BALL_Y_SPEED)
+    elif ball_right == right_line_distance or ball_left == PAD_W:
+        if ball_right > WIDTH / 2:
+            if c.coords(RIGHT_PAD)[1] < ball_center < c.coords(RIGHT_PAD)[3]:
+                bounce("strike")
+            else:
+                pass
+        else:
+            if c.coords(LEFT_PAD)[1] < ball_center < c.coords(LEFT_PAD)[3]:
+                bounce("strike")
+            else:
+                pass
+    else:
+        if ball_right > WIDTH / 2:
+            c.move(BALL, right_line_distance - ball_right, BALL_Y_SPEED)
+        else:
+            c.move(BALL, -ball_left + PAD_W, BALL_Y_SPEED)
+    if ball_top + BALL_Y_SPEED < 0 or ball_bot + BALL_Y_SPEED > HEIGHT:
+        bounce("ricochet")
+
 
 def move_pads():
-    PADS = {LEFT_PAD:LEFT_PAD_SPEED,
-            RIGHT_PAD:RIGHT_PAD_SPEED}
+    PADS = {LEFT_PAD: LEFT_PAD_SPEED, RIGHT_PAD: RIGHT_PAD_SPEED}
     for pad in PADS:
         c.move(pad, 0, PADS[pad])
-        if c.coords(pad)[1]< 0:
+        if c.coords(pad)[1] < 0:  # ракетка на місце
             c.move(pad, 0, -c.coords(pad)[1])
         elif c.coords(pad)[3] > HEIGHT:
             c.move(pad, 0, HEIGHT - c.coords(pad)[3])
@@ -51,9 +84,8 @@ def move_pads():
 def main():
     move_ball()
     move_pads()
-    root.after(30, main)
+    window.after(30, main)
 
-c.focus_set()
 
 def movement_handler(event):
     global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
@@ -66,7 +98,6 @@ def movement_handler(event):
     elif event.keysym == "Down":
         RIGHT_PAD_SPEED = PAD_SPEED
 
-c.bind("<KeyPress>", movement_handler)
 
 def stop_pad(event):
     global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
@@ -75,8 +106,10 @@ def stop_pad(event):
     elif event.keysym in ("Up", "Down"):
         RIGHT_PAD_SPEED = 0
 
-c.bind("<KeyRelease>", stop_pad)
 
+c.bind("<KeyPress>", movement_handler)
+c.bind("<KeyRelease>", stop_pad)
+c.focus_set()
 main()
 
-root.mainloop()
+window.mainloop()
